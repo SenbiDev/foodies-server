725bf3d4e93caf6be4705c8fd96c57367f6aecc7
SenbiDev <senbidev@gmail.com>
2023-05-22 06:19:03 +0700

    change cors config on main.ts & cors config on vercel.json

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-// import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
   const app = await NestFactory.create(AppModule);
-
-  // app.use((req, res, next) => {
-  //   res.header('Access-Control-Allow-Origin', '*');
-  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-  //   next();
-  // });
-
-  // app.enableCors({
-  //   allowedHeaders: '*',
-  //   origin : '*',
-  //   credentials: true,
-  //   methods : ['GET','POST','PUT','DELETE'],
-  // });
 
   app.enableCors();
 
