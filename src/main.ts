a376ea40a43d50e46fa3549809ab4540e62321dc
SenbiDev <senbidev@gmail.com>
2023-05-21 18:33:55 +0700

    change cors config 6x

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
+// import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   next();
+  // });
 
-  app.enableCors({
-    allowedHeaders: '*',
-    origin : '*',
-    credentials: true,
-    // methods : 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  });
+  // app.enableCors({
+  //   allowedHeaders: '*',
+  //   origin : '*',
+  //   credentials: true,
+  //   // methods : 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  // });
 
   await app.listen(3000);
 }
