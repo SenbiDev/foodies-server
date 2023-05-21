544cee78b9099856f59fbdefea1c379753b044db
SenbiDev <senbidev@gmail.com>
2023-05-21 18:44:41 +0700

    change cors config 8x

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
+// import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   next();
+  // });
 
   app.enableCors({
-    allowedHeaders: '*',
-    origin : '*',
-    credentials: true,
-    methods : 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // allowedHeaders: '*',
+    origin : ['https://foodies-server-eight.vercel.app'],
+    // credentials: true,
+    methods : ['GET','POST','PUT','DELETE'],
   });
 
   await app.listen(3000);
