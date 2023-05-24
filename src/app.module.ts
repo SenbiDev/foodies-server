cd5dc97b2dd85a38bd5f6a442b601994b57c0296
SenbiDev <senbidev@gmail.com>
2023-05-24 09:12:22 +0700

    add pipe dir inside product dir

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { FirebaseModule } from 'nestjs-firebase';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ParseIntPipe } from 'pipe/parse-int.pipe';
 import { CounterModule } from './counter/counter.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
@@ -39,6 +38,6 @@ import { InvoiceModule } from './invoice/invoice.module';
     InvoiceModule,
   ],
   controllers: [AppController],
-  providers: [AppService, ParseIntPipe],
+  providers: [AppService],
 })
 export class AppModule {}
