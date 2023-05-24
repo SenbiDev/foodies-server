3ce04c24b7ea2e2233491e17f40199f28b77405f
SenbiDev <senbidev@gmail.com>
2023-05-24 08:38:33 +0700

    change pipe index productService

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,4 +1,6 @@
-import { Module } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { ParseIntPipe } from 'pipe/parse-int.pipe';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { CategoryModule } from '../category/category.module';
@@ -6,7 +8,13 @@ import { TagModule } from '../tag/tag.module';
 
 @Module({
   imports: [CategoryModule, TagModule],
-  providers: [ProductService],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+    ProductService
+  ],
   controllers: [ProductController],
   exports: [ProductService],
 })
